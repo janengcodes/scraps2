84a2da8f3d322128df7b68c40d14aa7fa8b1c243
janengcodes <janeng@umich.edu>
2024-01-21 23:44:14 -0500

    messy logout

--- scraps/views/__init__.py
+++ scraps/views/__init__.py
@@ -8,7 +8,7 @@ from scraps.views.recipe import recipe
 from scraps.views.results import results
 from scraps.views.user import show_user
 from scraps.views.pantry_main import pantry_main
-from scraps.views.accounts import login, create 
+from scraps.views.accounts import login, create, logout
 # from scraps.views.sign_in import sign_in
 from scraps.views.accounts import show_accounts_login
 from scraps.views.recipes import get_recipes
--- scraps/views/accounts.py
+++ scraps/views/accounts.py
@@ -31,20 +31,20 @@ def show_accounts_sign_up():
 
 # HANDLE POST REQUESTS
 @scraps.app.route('/accounts/', methods=['POST'])
-def post_accounts():
-    """Process POST requests of type /accounts/<operation>/."""
-    op_name = flask.request.form.get('operation')
-    match op_name:
-        case 'create':
-            create()
-        case 'login':
-            login()
-        case 'logout':
-            logout()
-
+def post_account():
+    """Display /accounts/ route."""
+    operation = request.form.get('operation')
+    if operation == 'create':
+        create()
+    # elif operation == 'delete':
+    #     delete()
+    # elif operation == 'edit_account':
+    #     edit_account()
+    # elif operation == 'update_password':
+    #     update_password()
+    elif operation == 'login':
+        login()
     target = flask.request.args.get('target', '/')
-
-    # Always redirects to target after above logic
     return flask.redirect(target)
 
 
@@ -117,20 +117,64 @@ def create():
 
 
 def login():
-    """Authenticate a login into an account."""
-    username = flask.request.form["username"]
-    password = flask.request.form["password"]
+    """Login a user."""
+    if 'username' in flask.session:
+        return flask.redirect(flask.url_for('show_index'))
+    connection = scraps.model.get_db()
+    # Handle the login form submission
+    username = request.form.get('username')
+    password = request.form.get('password')
 
-    # authenticate with given username and password
-    logname = scraps.model.http_authenticate(flask.model.get_db,
-                                             username,
-                                             password)
+    # If the username or password fields are empty, abort(400).
+    if len(username) == 0 or len(password) == 0:
+        abort(400)
 
-    # log user in
-    flask.session['username'] = logname
+    # If username and password authentication fails, abort(403).
+
+    # username authentification
+    username_check = connection.execute('''
+        SELECT COUNT(*)
+        FROM users
+        WHERE username = ?
+    ''', (username,),).fetchone()
+    # If username doesn't exist
+    if username_check['COUNT(*)'] == 0:
+        abort(403)
+    # grab salt
+    real_password = connection.execute('''
+        SELECT password
+        FROM users
+        WHERE username = ?
+    ''', (username,),).fetchone()
+    parts = real_password['password'].split('$')
+    salt = parts[1]
+    algorithm = 'sha512'
+    # salt = uuid.uuid4().hex
+    hash_obj = hashlib.new(algorithm)
+    password_salted = salt + password
+    hash_obj.update(password_salted.encode('utf-8'))
+    password_hash = hash_obj.hexdigest()
+    password_db_string = "$".join([algorithm, salt, password_hash])
+    # if the password in the database doesn't match
+    # inputted in the form, abort(403)
+    # real_password['password'] is the password in the database
+    # password_db_string is the password inputted in the form
+    if password_db_string != real_password['password']:
+        abort(403)
+
+    # set a session cookie
+    flask.session['username'] = username
+
+    target = flask.request.args.get('target', '/')
+    return flask.redirect(target)
+    # Handle other cases (e.g., invalid login)
 
 
+@scraps.app.route('/accounts/logout/', methods=['POST'])
 def logout():
-    """Logout and clear cookies from an account."""
-    flask.session.clear()
+    if 'username' in flask.session:
+        flask.session.clear()
+    # return flask.redirect(flask.url_for('show_index'))
+    target = flask.request.args.get('target', '/')
+    return flask.redirect(target)
 
