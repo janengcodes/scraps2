88c3897424261be36765cd72f3592ee032e660d3
Jane Nguyen <janeng@umich.edu>
2024-04-16 18:46:44 -0400

    apr 16

--- scraps/js/NavBar.jsx
+++ scraps/js/NavBar.jsx
@@ -5,26 +5,6 @@ import Navbar from "react-bootstrap/Navbar";
 import { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 
-
-
-
-  /* <Nav className="me-auto">
-  <Nav.Link href="#features">Features</Nav.Link>
-  <Nav.Link href="#pricing">Pricing</Nav.Link>
-  <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-    <NavDropdown.Item href="#action/3.2">
-      Another action
-    </NavDropdown.Item>
-    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-    <NavDropdown.Divider />
-    <NavDropdown.Item href="#action/3.4">
-      Separated link
-    </NavDropdown.Item>
-  </NavDropdown>
-</Nav> */
-
-
 export default function LoggedInNavBar({}) {
   const [user, setUser] = useState("");
 
