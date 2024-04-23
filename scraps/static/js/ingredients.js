9ba110209ef5074e37a425699e466117c8a50d3e
Jane Nguyen <janeng@umich.edu>
2024-04-23 15:37:44 -0400

    select ingredients frontend

--- scraps/static/js/ingredients.js
+++ scraps/static/js/ingredients.js
@@ -20,6 +20,17 @@ function deleteGrids(clickedButton) {
   elements.set("grainsButton", ["grains", "grainsHeading"]);
 
   for (var [key, value] of elements) {
+
+    var buttons = document.querySelectorAll('.button-2');
+    buttons.forEach(function(btn) {
+      btn.classList.remove('button-clicked');
+    });
+    buttons.forEach(function(btn) {
+      btn.classList.remove('produce-init');
+    });
+    clickedButton.classList.add('button-clicked');
+
+
     var ingredient = document.getElementById(value[0]);
     var heading = document.getElementById(value[1]);
     if (key === clickedButton.id) {
