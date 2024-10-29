18b09753803162ceaf86be35e27dbfc6c9cbd1b0
Nicky Nguyen <nickyngu@umich.edu>
2024-10-29 18:42:52 -0400

    navbar saved_recipes

--- scraps/js/NavBar.jsx
+++ scraps/js/NavBar.jsx
@@ -33,12 +33,6 @@ export default function LoggedInNavBar({}) {
       <Container>
         <Navbar.Brand href="/">
           Scraps{" "}
-          {/* <img
-            src="/static/images/carrot.png"
-            className="carrot"
-            style={{ width: "50px", height: "50px" }}
-            alt="carrot icon"
-          /> */}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
