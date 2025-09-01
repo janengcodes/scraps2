1ea923d7ff5bdbaaeccfb0024b74455cd499c130
Jane Nguyen <janeng@umich.edu>
2025-09-01 17:50:54 -0600

    basic styling for ingredient cells

--- scraps/js/PantryTable.jsx
+++ scraps/js/PantryTable.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import "../static/css/pantry-table.css";
+
 import "../static/css/globals.css";
 import "../static/css/style.css";
+import "../static/css/pantry-table.css";
 import axios from "axios";
 
 export default function UserPantryTable() {
   console.log("Pantry table called");
   return (
+    <>
+  
     <div className="pantry-layout">
       <div className="ingredients-cell">
         <div className="main-ingredient-form">
@@ -32,7 +35,7 @@ export default function UserPantryTable() {
 
         <div className="ingredient-section">
           <div className="ingredient-heading">
-            <h2 className="space-mono-bold">Veggies</h2>
+            <p className="space-mono-bold">Veggies</p>
           </div>
           <div className="ingredient-list">
             <ul className="a be-vietnam-pro-smaller">
@@ -72,25 +75,25 @@ export default function UserPantryTable() {
 
         <div className="ingredient-section">
           <div className="ingredient-heading">
-            <h2 className="space-mono-bold">Fruits</h2>
+            <p className="space-mono-bold">Fruits</p>
           </div>
         </div>
 
         <div className="ingredient-section">
           <div className="ingredient-heading">
-            <h2 className="space-mono-bold">Protein</h2>
+            <p className="space-mono-bold">Protein</p>
           </div>
         </div>
 
         <div className="ingredient-section">
           <div className="ingredient-heading">
-            <h2 className="space-mono-bold">Grains</h2>
+            <p className="space-mono-bold">Grains</p>
           </div>
         </div>
 
         <div className="ingredient-section">
-          <div className="ingredient-heading">
-            <h2 className="space-mono-bold">Other</h2>
+          <div className="ingredient-heading other-ingredients">
+            <p className="space-mono-bold">Other</p>
           </div>
         </div>
       </div>
@@ -98,23 +101,24 @@ export default function UserPantryTable() {
       <div className="pantry-info-cell">
         <div className="pie-chart">
           <div className="pie-chart-heading">
-            <h2 className="space-mono-bold">Pantry</h2>
+            <p className="space-mono-bold">Stats</p>
           </div>
         </div>
         <div className="cuisine-matches">
           <div className="cuisine-matches-heading">
-            <h2 className="space-mono-bold">Cuisine Matches</h2>
+            <p className="space-mono-bold">Looking for a new cuisine?</p>
           </div>
         </div>
       </div>
 
       <div className="in-season-ingredients-cell">
         <div className="isi-heading">
-          <h2 className="space-mono-bold">
+          <p className="space-mono-bold">
             In Season Ingredients for Denver, Colorado
-          </h2>
+          </p>
         </div>
       </div>
     </div>
+    </>
   );
 }
